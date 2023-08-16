b3482f7fefad4a21c2fcbcad04fde8c7bfbd32f1
Kulpot <kulpotwahu@gmail.com>
2023-08-16 19:06:40 +0800

    Add project files.

---
+++ Inferring Anonymous Type Property Names/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+//ref link:https://www.youtube.com/watch?v=HVF3k_H171w&list=PL259BC2977085CF37&index=6
+//
+
+class MainClass
+{
+    static void Main()
+    {
+        string firstName = "Kulpot";
+        string lastName = "King";
+
+        //var person = new { firstName, lastName };
+        var person = new { firstName, lastName, FullName = firstName + " " + lastName };
+        Console.WriteLine(person.firstName);
+        Console.WriteLine(person.lastName);
+        Console.WriteLine(person);
+
+        //var person = new { FirstName = "Kulpot", LastName = "King" };
+    }
+}
